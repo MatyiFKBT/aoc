d610a408e1e8f7f18bff72339a5760acae45a5f5
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2023-12-01 17:23:28 +0100

    refactor: day01

--- day_01.ts
+++ day_01.ts
@@ -7,9 +7,7 @@ export const part1 = (input: string) => {
   let sum = 0;
   input.split("\n").forEach((line) => {
     const chars = line.split("");
-    const nums = chars.map((char) => parseInt(char)).filter((num) =>
-      !isNaN(num)
-    );
+    const nums = chars.map((char) => parseInt(char)).filter(Boolean);
     sum += parseDigits(nums);
   });
   return sum;
@@ -31,9 +29,14 @@ export const part2 = (input: string) => {
       "nine",
     ]
       .reduce(
-        (acc, word, index) => acc.replaceAll(word, word + (index + 1) + word),
+        (prev, word, index) => prev.replaceAll(word, word + (index + 1) + word),
+        // this is a bit hacky, but it works
+        // turns "one" into "one1one"
+        // and twone into "two2twone1one"
+        // parsing this way returns both numbers
         line,
       )
+      // same as part 1:
       .split("")
       .map(Number)
       .filter(Boolean);
