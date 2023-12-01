bba9823eefa78aae518299cd21677fc21ce8721e
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2023-12-01 14:13:57 +0100

    chore: add 2023 day 01

--- day_01.ts
+++ day_01.ts
@@ -1,32 +1,43 @@
+const parseDigits = (nums: number[] | []) => {
+  if (nums.length === 0) return 0;
+  return parseInt(`${nums[0]}${nums[nums.length - 1]}`);
+};
+
 export const part1 = (input: string) => {
-  const blocks = input.trim().split("\n\n");
-  let maxCalories = 0;
-  const calories: number[] = [];
-  blocks.forEach((block) => {
-    let currCalorie = 0;
-    block.split("\n").forEach((c) => {
-      currCalorie += parseInt(c);
-    });
-    calories.push(currCalorie);
+  let sum = 0;
+  input.split("\n").forEach((line) => {
+    const chars = line.split("");
+    const nums = chars.map((char) => parseInt(char)).filter((num) =>
+      !isNaN(num)
+    );
+    sum += parseDigits(nums);
   });
-  maxCalories = Math.max(...calories);
-  return maxCalories;
+  return sum;
 };
 
 export const part2 = (input: string) => {
-  const blocks = input.split("\n\n");
-  const calories: number[] = [];
-  blocks.forEach((block) => {
-    let currCalorie = 0;
-    block.split("\n").forEach((c) => {
-      currCalorie += parseInt(c);
-    });
-    calories.push(currCalorie);
-  });
+  let sum = 0;
 
-  console.log(
-    calories.sort(function (a, b) {
-      return b - a;
-    }).slice(0, 3).reduce((partialSum, a) => partialSum + a, 0),
-  );
+  input.split("\n").forEach((line) => {
+    const digits = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ]
+      .reduce(
+        (acc, word, index) => acc.replaceAll(word, word + (index + 1) + word),
+        line,
+      )
+      .split("")
+      .map(Number)
+      .filter(Boolean);
+    sum += parseDigits(digits);
+  });
+  return sum;
 };
