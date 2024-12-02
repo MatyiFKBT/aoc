1af0cc1ee21773a62e40bbf6b57ffef5062c1628
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2024-12-02 06:32:23 +0100

    chore: add day2 (#9)
    * chore: add day2
    
    * chore: add day2

---
+++ src/2024/02/part1.ts
@@ -0,0 +1,23 @@
+export default function solution(input: string) {
+    const rows = input.split("\n").filter((v) => v != "").map((w) =>
+        w.split(' ').map(num => parseInt(num))
+    );
+    const safe_rows = rows.filter((row) => {
+        const diffs = row.map((item, index) => item - row[index - 1]);
+        diffs.shift()
+        const is_safe = diffs.every(diff => diff >= -3 && diff < 0) || diffs.every(diff => diff > 0 && diff <= 3)
+        return is_safe;
+    })
+
+    return safe_rows.length
+}
+
+export const tests = [
+    [`
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`, 2],
+]
---
+++ src/2024/02/part2.ts
@@ -0,0 +1,38 @@
+export default function solution(input: string) {
+    const rows = input.split("\n").filter((v) => v != "").map((w) =>
+        w.split(' ').map(num => parseInt(num))
+    );
+    const is_row_safe = (row: number[]) => {
+        const diffs = row.map((item, index) => item - row[index - 1]);
+        diffs.shift()
+        const is_safe = diffs.every(diff => diff >= -3 && diff < 0) || diffs.every(diff => diff > 0 && diff <= 3)
+        if (is_safe) {
+            return true
+        };
+    }
+    const safe_rows = rows.filter(row => {
+        if (is_row_safe(row)) {
+            return true
+        }
+
+        for (let index = 0; index < row.length; index++) {
+            const new_row = [...row];
+            new_row.splice(index, 1);
+            if (is_row_safe(new_row)) {
+                return true
+            }
+        }
+    });
+
+    return safe_rows.length
+}
+
+export const tests = [
+    [`
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`, 4],
+]
