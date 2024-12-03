0e665c320ed51b586736dfe44d2f8ed3e3f0dfbe
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2024-12-03 14:50:18 +0100

    feat: test and solve the recently modified file (#12)
    also ran deno format on all ts files

--- utils/solve.ts
+++ utils/solve.ts
@@ -1,10 +1,21 @@
 import { submit } from "./api/submit.ts";
 import { run } from "./run.ts";
 
-const SESSION = Deno.env.get("SESSION")!; // TS-Config disagrees with its existence even if I check it
+const SESSION = Deno.env.get("SESSION")!;
 const YEAR = Deno.args[0] ?? new Date().getFullYear();
 const DATE = Deno.args[1] ?? new Date().getDate();
-const PART = Deno.args[2] ?? 1;
-
+const formattedDate = `0${DATE}`.slice(-2);
+const files = [...Deno.readDirSync(`./src/${YEAR}/${formattedDate}`)].map((
+  e,
+) => e.name)
+  .map((filename) => {
+    return {
+      filename,
+      mtime: Deno.statSync(`./src/${YEAR}/${formattedDate}/${filename}`).mtime,
+    };
+  });
+const mostRecent =
+  files.sort((a, b) => b.mtime!.getTime() - a.mtime!.getTime())[0];
+const PART = Deno.args[2] ?? mostRecent.filename.match(/[\d]/)![0];
 const ATTEMPT = await run(YEAR, DATE, PART);
 await submit(YEAR, DATE, PART, SESSION, ATTEMPT);
